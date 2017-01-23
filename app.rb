8424e8a01faedd253bc5b347e671f58b19d245f7
i-yuuki <yuuki_16n1100172@nnn.ed.jp>
2017-01-23 14:46:07 +0900

    Add save/fetch entries

--- app.rb
+++ app.rb
@@ -1,6 +1,14 @@
 require "bundler/setup"
 Bundler.require(:default)
 
+Dir["models/*.rb"].each do |model|
+  require_relative model
+end
+
+Dir["repositories/*.rb"].each do |model|
+  require_relative model
+end
+
 class App < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
@@ -8,9 +16,29 @@ class App < Sinatra::Base
   configure do
     set :views, settings.root + "/views"
   end
+  helpers do
+    def entry_repository
+      @@entry_repository ||= EntryRepository.new
+    end
+  end
   get "/" do
     @name = "ドラえもん"
     slim :index
+  end
+  get "/entries/new" do
+    slim :new
+  end
+  post "/entries" do
+    entry = Entry.new
+    entry.title = params[:title]
+    entry.body = params[:body]
+    id = entry_repository.save(entry)
+    
+    redirect to("/entries/#{id}")
+  end
+  get "/entries/:id" do
+    @entry = entry_repository.fetch(params[:id].to_i)
+    slim :entry
   end
   get "/:name" do
     @name = params[:name]
---
+++ repositories/entry_repository.rb
@@ -0,0 +1,13 @@
+class EntryRepository
+  def initialize
+    @entries = []
+  end
+  def save(entry)
+    @entries.push(entry)
+    return @entries.length - 1
+  end
+
+  def fetch(id)
+    return @entries[id]
+  end
+end
