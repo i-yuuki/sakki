bd13d2530a6c9530b365f99b7d8156b16873a178
i-yuuki <yuuki_16n1100172@nnn.ed.jp>
2017-01-23 16:32:20 +0900

    Hello sql!

--- app.rb
+++ app.rb
@@ -16,9 +16,15 @@ class App < Sinatra::Base
   configure do
     set :views, settings.root + "/views"
   end
+  def self.database_config
+    YAML.load_file("config/database.yml")[ENV['RACK_ENV'] || 'development']
+  end
+  def self.database
+    @database ||= Mysql2::Client.new(database_config)
+  end
   helpers do
     def entry_repository
-      @@entry_repository ||= EntryRepository.new
+      @@entry_repository ||= EntryRepository.new(App.database)
     end
   end
   get "/" do
--- repositories/entry_repository.rb
+++ repositories/entry_repository.rb
@@ -1,20 +1,39 @@
 class EntryRepository
   include Enumerable
   
-  def initialize
-    @entries = []
+  def initialize(db)
+    @db = db
   end
   
   def save(entry)
-    @entries.push(entry)
-    return @entries.length - 1
+    columns = ['title', 'body']
+    @db.prepare("INSERT INTO `entries` (#{columns.join(", ")}) VALUES (?, ?)").execute(entry.title, entry.body)
+    return @db.last_id
   end
 
   def fetch(id)
-    return @entries[id]
+    res = @db.prepare("SELECT * FROM `entries` WHERE `id` = ?").execute(id)
+
+    data = res.first
+    entry = Entry.new
+    entry.title = data["title"]
+    entry.body = data["body"]
+
+    return entry
   end
   
   def each(&block)
-    @entries.each(&block)
+    entries = []
+    query = "SELECT * FROM `entries`"
+    res = @db.query(query)
+
+    res.each do |row|
+      entry = Entry.new
+      entry.title = row["title"]
+      entry.body = row["body"]
+      entries.push(entry)
+    end
+
+    entries.each(&block)
   end
 end
